bbd5af5a50bbe4a6f6fc37c8a6277163b31518de
Mike Sanders <msanders331@gmail.com>
2022-02-28 22:54:13 -0800

    added all files to be included to index.js including class constructores, inquirer and fs.

---
+++ index.js
@@ -0,0 +1,7 @@
+//classes required
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+
+const inquirer = require('inquirer');
+const fs = require('fs');
