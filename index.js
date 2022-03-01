3d3da9e8664d08b4a59f82b056beea18b355caa7
Mike Sanders <msanders331@gmail.com>
2022-03-01 00:18:05 -0800

    added inquirer functions: menu for user choices when adding or finishing their profile + addintern and add engineer functions.

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const Engineer = require('./lib/Engineer');
 //inquire npm used for user prompting and filesystem (FS) used for writing to the HTML File.
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { choices } = require('yargs');
 
 //array which will be used to store the team's profile from inquirer prompt.
 const teamProfile = [];
 
-const managerQuestions = [
+const managerQs = [
     {
         type: 'input',
         name: 'name',
@@ -33,11 +34,103 @@ const managerQuestions = [
     }
 ]
 
+//prompt questions for the engineer inquirer prompt 
+const engineerQs = [
+    {
+        type: 'input',
+        name: 'name',
+        message: `What's the engineer's name?`,
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: `What's the engineer's id?`,
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: `What's the engineer's email address?`
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: `What's the engineer's github user name?`
+    }
+];
+//prompt questions for the intern inquirer prompt
+const internQs = [
+    {
+        type: 'input',
+        name: 'name',
+        message: `What's the intern's name?`,
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: `What's the intern's id?`,
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: `What's the intern's email address?`
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: `What's the name of the intern's school?`
+    }
+];
+//function to create a new class instance of manager when called.
 const addManager = () => {
-    return inquirer.prompt(managerQuestions)
-    .then((answers)=>{
+    return inquirer.prompt(managerQs)
+    .then((answers) => {
         const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
 
         teamProfile.push(manager);
     });
+}
+
+const addEngineer= () => {
+    return inquirer.prompt(engineerQs)
+    .then((answers) => {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+
+        teamProfile.push(engineer);
+    })
+}
+
+const addIntern = () => {
+    return inquirer.prompt(internQs)
+    .then((answers) => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        teamProfile.push(intern);
+    })
+}
+
+//menu of choices for the user when adding folks to their team
+const menuQs = [
+    {
+        type: 'list',
+        name: 'choice',
+        message: `What would you like to do next? Add someone to your team or are you all done building your team?`,
+        choices: ['Add an Engineer', 'Add an Intern', 'Finished Building']
+    }
+];
+
+const menu = () => {
+    return inquirer.prompt(menuQs)
+    .then((answers) => {
+        //user wants to add an engineer to their team
+        if(answers.choices === 'Add an Engineer'){
+            addEngineer();
+        }
+        //user wants to add an intern to their team
+        else if(answers.choices === 'Add an Intern'){
+            addIntern();
+        }
+        //user was done building call the write to HTML file function
+        else{
+            return;
+        }
+    })
 }
