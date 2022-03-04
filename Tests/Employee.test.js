a8ae31bbeefd76df27743afaf33cae5d01c7cfac
Mike Sanders <msanders331@gmail.com>
2022-03-03 21:43:11 -0800

    wrote test for employee class found in tests folder.

---
+++ Tests/Employee.test.js
@@ -0,0 +1,20 @@
+//import the employee class constructor and file
+const Employee = require('../lib/Employee');
+
+test('creates a new employee object with a name, id, and email', () =>{
+    const newEmployee = new Employee('Mike', 52, 'mike@mikecsanders.com');
+
+    //testing object creation
+    expect(newEmployee.name).toBe('Mike');
+    expect(newEmployee.id).toBe(52);
+    expect(newEmployee.email).toBe('mike@mikecsanders.com')
+
+    //testing the get name function
+    expect(newEmployee.getName()).toEqual(expect.any(String));
+
+    //testing the get email function
+    expect(newEmployee.getEmail()).toEqual(expect.any(String));
+
+    //testing the get role function
+    expect(newEmployee.getRole()).toBe('Employee');
+});
